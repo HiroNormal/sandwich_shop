451c9e6cebf3fc1f9979fc8ac5b138cedfdd4590
Hiro <Hearthwave@gmail.com>
2025-10-10 15:20:53 +0100

    Text Changes

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -104,13 +104,13 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('Welcome to the Sandwich shop!'),
             Text(
-              '$_counter',
+              'You have ordered $_counter sandwiches.',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
-        ),
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
